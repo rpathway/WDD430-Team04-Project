2709fafd55e6c2c21b3be92276bda14beb6caaca
rpathway <218506808+rpathway@users.noreply.github.com>
2026-06-10 00:12:57 -0400

    Added home, product, and product detail pages.

---
+++ app/ui/button.tsx
@@ -0,0 +1,18 @@
+import clsx from 'clsx';
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  children: React.ReactNode;
+}
+
+export function Button({ children, className, ...rest }: ButtonProps) {
+  return (
+    <button
+    {...rest}
+    className={clsx(
+      `flex rounded-lg bg-blue-400 px-4 py-2 text-sm text-center transition-colors hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-500 active:bg-blue-600 aria-disabled:cursor-not-allowed aria-disabled-opacity-50`,
+      className,
+    )}>
+      {children}
+    </button>
+  )
+}
---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { Playfair_Display, Inter } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+export const playfair = Playfair_Display({
+  subsets: ['latin'],
+  style: ['normal', 'italic']
+})
